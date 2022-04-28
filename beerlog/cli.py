aba6aced4a8d692e2aceee96fbbfc8266cbdf2d4
Luiz Lima <umluizlima@gmail.com>
2022-04-28 20:10:35 -0300

    Add initial add and list commands to CLI

--- beerlog/cli.py
+++ beerlog/cli.py
@@ -1,5 +1,22 @@
-from .config import settings
+import typer
 
 
-def main():
-    print("Hello from", settings.NAME)
+main = typer.Typer(help="Beer Management Application")
+
+
+@main.command("add")
+def add(
+    name: str,
+    style: str,
+    flavor: int = typer.Option(...),
+    image: int = typer.Option(...),
+    cost: int = typer.Option(...),
+):
+    """Adds a new beer to database."""
+    print(name, style)
+
+
+@main.command("list")
+def list_beers(style: str):
+    """Lists beers in database."""
+    print(style)
