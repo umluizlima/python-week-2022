9dcae5eea383089c3fea238c80a07adfad3197bf
Luiz Lima <umluizlima@gmail.com>
2022-04-28 20:12:22 -0300

    Add get_session function to database

--- beerlog/database.py
+++ beerlog/database.py
@@ -1,4 +1,4 @@
-from sqlmodel import create_engine
+from sqlmodel import create_engine, Session
 
 from beerlog.config import settings
 from beerlog import models
@@ -6,3 +6,7 @@ from beerlog import models
 engine = create_engine(settings.database.url)
 
 models.SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    return Session(engine)
