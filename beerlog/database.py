807539036fd3c9ea25e82124023a49b46c1ce89d
Luiz Lima <umluizlima@gmail.com>
2022-04-28 20:28:36 -0300

    Hide database warnings

--- beerlog/database.py
+++ beerlog/database.py
@@ -1,7 +1,14 @@
+import warnings
+from sqlalchemy.exc import SAWarning
+from sqlmodel.sql.expression import Select, SelectOfScalar
 from sqlmodel import create_engine, Session
 
 from beerlog.config import settings
 from beerlog import models
+
+warnings.filterwarnings("ignore", category=SAWarning)
+SelectOfScalar.inherit_cache = True
+Select.inherit_cache = True
 
 engine = create_engine(settings.database.url)
 
