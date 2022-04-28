f00bed38ee5abc3e761653444a95f6fe42bde74d
Luiz Lima <umluizlima@gmail.com>
2022-04-28 19:21:35 -0300

    Add validator, rate and date

--- beerlog/models.py
+++ beerlog/models.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+from statistics import mean
 from typing import Optional
 
+from pydantic import validator
 from sqlmodel import SQLModel, Field
 
 
@@ -10,6 +13,22 @@ class Beer(SQLModel, table=True):
     flavor: int
     image: int
     cost: int
+    rate: int = 0
+    date: datetime = Field(default_factory=datetime.now)
+
+    @validator("flavor", "image", "cost")
+    def validate_ratings(cls, v, field):
+        if v < 1 or v > 10:
+            raise RuntimeError(f"{field.name} must be between 1 and 10")
+        return v
+
+    @validator("rate", always=True)
+    def calculate_rate(cls, v, values):
+        rate = mean([values["flavor"], values["image"], values["cost"]])
+        return int(rate)
 
 
-brewdog = Beer(name="Brewdog", style="NEIPA", flavor=6, image=8, cost=8)
+try:
+    brewdog = Beer(name="Brewdog", style="NEIPA", flavor=6, image=8, cost=8)
+except RuntimeError:
+    print("error found")
