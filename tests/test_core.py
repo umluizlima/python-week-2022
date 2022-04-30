1b7e1dd4d1f9d42e93190f08a14bf33a264fbb17
Luiz Lima <umluizlima@gmail.com>
2022-04-29 22:39:11 -0300

    Fix unit test

--- tests/test_core.py
+++ tests/test_core.py
@@ -6,5 +6,9 @@ def test_add_beer_to_database():
 
 
 def test_get_beers_from_database():
+    # Arrange
+    add_beer_to_database("Blue Moon", "Witbier", 10, 3, 6)
+    # Act
     results = get_beers_from_database()
+    # Assert
     assert len(results) > 0
