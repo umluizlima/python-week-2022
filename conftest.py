4ab3803c04a819e71c14bb05466c3bb0f5969789
Luiz Lima <umluizlima@gmail.com>
2022-04-29 22:38:15 -0300

    Add conftest

---
+++ conftest.py
@@ -0,0 +1,16 @@
+from unittest.mock import patch
+
+import pytest
+from sqlmodel import create_engine
+
+from beerlog import models
+
+
+@pytest.fixture(autouse=True, scope="function")
+def each_test_uses_separate_database(request):
+    tmpdir = request.getfixturevalue("tmpdir")
+    test_db = tmpdir.join("beerlog.test.db")
+    engine = create_engine(f"sqlite:///{test_db}")
+    models.SQLModel.metadata.create_all(bind=engine)
+    with patch("beerlog.database.engine", engine):
+        yield
